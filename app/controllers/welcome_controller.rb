e215c750c2603e578e40db89afd343e111dd8883
Takeshi Kishi <takeya0x86@gmail.com>
2020-05-17 13:39:30 +0900

    add root redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,9 @@
 class WelcomeController < ApplicationController
+  def index
+    if user_signed_in?
+      redirect_to movies_url
+    else
+      redirect_to new_user_session_url
+    end
+  end
 end
