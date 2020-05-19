826698adee24acc681fb2f15b8a9f88ab2bdc894
Takeshi Kishi <takeya0x86@gmail.com>
2020-05-20 00:54:01 +0900

    fix order

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,7 +5,7 @@ class DirectorsController < ApplicationController
   # GET /directors
   # GET /directors.json
   def index
-    @directors = Director.all
+    @directors = Director.order(:name)
   end
 
   # GET /directors/1
--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -5,7 +5,7 @@ class SeriesController < ApplicationController
   # GET /series
   # GET /series.json
   def index
-    @series = Series.all
+    @series = Series.order(:name)
   end
 
   # GET /series/1
